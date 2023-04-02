1576c3b427745154debcc9bc06ff94bd9621bf9d
Richard Chen <rcchen97@gmail.com>
2023-04-01 19:40:38 -0500

    implemented requested changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import "./styles.css"
+import React from 'react'
+import {useState} from 'react'
+import {useEffect} from 'react'
+
+
 
 function App() {
+
+  const[records, setRecords] = useState([]) //We create an array of two items using React's useState hook. 'setRecords' will be used to change the
+  
+  function(id, completed){
+    setRecords((prevData) =>
+      prevData.map((list) =>
+        list.id === id ? { ...list, completed: !completed } : list
+      )
+    );
+  };
+
+  useEffect(() =>{
+    
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(data => setRecords(data))
+      .catch(err => console.log(err))
+      
+   
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {records.map((list, index)=> (
+          <li id={list.id} key={index} className={`container ${list.completed ? "strike" : ""}`}>
+          userId: {list.userId} <br />
+          id: {list.id} <br />
+          title: {list.title} <br />
+          completed: {String(list.completed) } 
+          <input type="checkbox" checked={list.completed} onChange={() => toggleComplete(list.id, list.completed)}/>
+          </li>
+        ))}
+      </ul>
     </div>
+    
   );
+  
 }
 
 export default App;
